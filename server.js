33a4e267dbbf649e0adf388356411ab0d2760ad5
Mandy2114 <mandydecamp1213@gmail.com>
2024-09-30 11:14:21 -0400

    updated, did not do css

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
+const session = require("express-session");
 const authController = require("./controllers/auth.js");
 
 // Set the port from environment variable or default to 3000
@@ -19,12 +20,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
+app.use(methodOverride("_method"));
+app.use(morgan('dev'));
+// new
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.use("/auth", authController);
 
 // server.js - GET /
-app.get("/", async (req, res) => {
-  res.render("index.ejs");
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    user: req.session.user,
+  });
+});
+
+app.get("/vip-lounge", (req, res) => {
+  if (req.session.user) {
+    res.send(`Welcome to the party ${req.session.user.username}.`);
+  } else {
+    res.send("Sorry, no guests allowed.");
+  }
 });
 
 mongoose.connection.on("connected", () => {
